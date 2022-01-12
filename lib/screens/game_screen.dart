28c84987778ca5c5d0e4229adb3d00793533a25a
Divyansh Joshi <divyanshjoshi20@gmail.com>
2022-01-12 21:08:56 +0530

    Refined Game Logic, gotta work on app logic now

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -15,6 +15,7 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  // Some display logic
   Future<bool> _onBackButtonPressed() async {
     return await showDialog(
             context: context,
@@ -22,32 +23,6 @@ class _GameScreenState extends State<GameScreen> {
               return const ExitGameDialog();
             }) ??
         false;
-  }
-
-  //  In Game Logic
-
-  List<ButtonMarker> board = [];
-  ButtonMarker? marker;
-  List<int> availableSpaces = [];
-
-  void _countAvailbleSpaces() {
-    availableSpaces = [];
-    for (int i = 0; i < board.length; i++) {
-      if (board[i] == ButtonMarker.available) {
-        availableSpaces.add(i);
-      }
-    }
-  }
-
-  void _runPlayersTurn(int i) {
-    setState(() {
-      board[i] = ButtonMarker.player;
-    });
-    dev.log('Player marked X on $i\n');
-
-    _checkWinner();
-
-    _runComputersTurn();
   }
 
   void _restartGame() {
@@ -85,50 +60,63 @@ class _GameScreenState extends State<GameScreen> {
         });
   }
 
+  //  In Game Logic functions begin here:
+
+  List<ButtonMarker> board = [];
+  List<int> _availableSpaces = [];
+  bool _isMatchFinished = false;
+  var _winnerMessage = '';
+
+  // Helper function to count the remaining spaces, so that the computer can mark its turn on one of these.
+  void _countAvailbleSpaces() {
+    _availableSpaces = [];
+    for (int i = 0; i < board.length; i++) {
+      if (board[i] == ButtonMarker.available) {
+        _availableSpaces.add(i);
+      }
+    }
+  }
+
+// Runs when the player presses on an available space on the screen
+  void _runPlayersTurn(int i) {
+    setState(() {
+      board[i] = ButtonMarker.player;
+    });
+    dev.log('Player marked X on $i\n');
+
+    _checkWinner();
+    if (_isMatchFinished) {
+      return;
+    }
+    _runComputersTurn();
+  }
+
+//Runs after the player's turn
   void _runComputersTurn() {
     _countAvailbleSpaces();
-    dev.log('Available Spaces: $availableSpaces');
+    dev.log('Available Spaces: $_availableSpaces');
+    if (_availableSpaces.isEmpty) return;
     Random random = Random();
-    int randomNumber = random.nextInt(availableSpaces.length);
+    int randomNumber = random.nextInt(_availableSpaces.length);
     dev.log('randomNumber = $randomNumber');
     setState(() {
-      board[availableSpaces[randomNumber]] = ButtonMarker.computer;
+      board[_availableSpaces[randomNumber]] = ButtonMarker.computer;
     });
-    dev.log('Computer marked O on ${availableSpaces[randomNumber]}\n');
-
+    dev.log('Computer marked O on ${_availableSpaces[randomNumber]}\n');
     _checkWinner();
   }
 
-  void _announceWinner(ButtonMarker marker) {
-    var winnerMessage = '';
-    if (marker == ButtonMarker.available) {
-      winnerMessage = 'Draw!';
-    } else if (marker == ButtonMarker.player) {
-      winnerMessage = 'Player Wins!';
-    } else {
-      winnerMessage = 'Computer Wins';
-    }
-  }
-
   void _checkWinner() {
-    int i = 0;
+    _countAvailbleSpaces();
 
-    board.forEach((element) {
-      // Counting the available spaces, henceforth the number of total turns
-      if (element == ButtonMarker.available) {
-        i++;
-      }
-    });
     // If the avaiable spaces are more than four, i.e, if the total turns yet are less than 5, there possibly cannot be a winner yet.
-    if (i > 4) {
+    if (_availableSpaces.length > 4) {
       return;
     }
-    if (i == 0) {
-      // Asking to announce a draw
+    // Asking to announce a draw
+    else if (_availableSpaces.isEmpty) {
       _announceWinner(ButtonMarker.available);
-    }
-
-    if (checkRow(0)) {
+    } else if (checkRow(0)) {
       _announceWinner(board[0]);
     } else if (checkRow(3)) {
       _announceWinner(board[3]);
@@ -140,27 +128,45 @@ class _GameScreenState extends State<GameScreen> {
       _announceWinner(board[1]);
     } else if (checkColumn(2)) {
       _announceWinner(board[2]);
-    } else if (board[0] == board[4] &&
-        board[4] == board[8]) // Checking for Diagonal 1
-    {
+    }
+    // Checking for Diagonal 1
+    else if (board[0] == board[4] && board[4] == board[8]) {
       _announceWinner(board[0]);
-    } else if (board[2] == board[4] &&
-        board[4] == board[6]) //Checkin for Diagonal 2
-    {
+    }
+    //Checking for Diagonal 2
+    else if (board[2] == board[4] && board[4] == board[6]) {
       _announceWinner(board[0]);
     }
   }
 
+// Annouces the winner
+  void _announceWinner(ButtonMarker marker) {
+    if (marker == ButtonMarker.available) {
+      _winnerMessage = 'Draw!';
+    } else if (marker == ButtonMarker.player) {
+      _winnerMessage = 'Player Wins!';
+    } else {
+      _winnerMessage = 'Computer Wins';
+    }
+    dev.log('\n *************** $_winnerMessage ***************\n');
+    setState(() {
+      _isMatchFinished = true;
+    });
+  }
+
+  // Helper function to check if a row is entirely marked by anyone
   bool checkRow(int firstIndex) {
     return (board[firstIndex] == board[firstIndex + 1]) &&
         (board[firstIndex + 1] == board[firstIndex + 2]);
   }
 
+  // Helper function to check if a column is entirely marked by anyone
   bool checkColumn(int firstIndex) {
     return (board[firstIndex] == board[firstIndex + 3]) &&
         (board[firstIndex + 3] == board[firstIndex + 6]);
   }
 
+// Function to reset the game board.
   void _resetBoard() {
     board = [];
     for (int i = 0; i < 9; i++) {
@@ -183,94 +189,135 @@ class _GameScreenState extends State<GameScreen> {
     return WillPopScope(
       onWillPop: _onBackButtonPressed,
       child: Scaffold(
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: !_isMatchFinished
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
                         children: [
-                          Expanded(
-                            child: Text(
-                              'Player',
-                              style:
-                                  Theme.of(context).primaryTextTheme.headline2,
-                              textAlign: TextAlign.center,
-                            ),
+                          Spacer(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'Player',
+                                  style: Theme.of(context)
+                                      .primaryTextTheme
+                                      .headline2,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Text(
+                                'VS',
+                                style: Theme.of(context)
+                                    .primaryTextTheme
+                                    .bodyText2,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  'Computer',
+                                  style: Theme.of(context)
+                                      .primaryTextTheme
+                                      .headline2,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            'VS',
-                            style: Theme.of(context).primaryTextTheme.bodyText2,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Computer',
-                              style:
-                                  Theme.of(context).primaryTextTheme.headline2,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
+                          const SizedBox(height: 20),
                         ],
                       ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-                Stack(children: [
-                  Container(
-                    // padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      // color: Colors.red.withOpacity(0.4),
                     ),
-                    height: _deviceWidth - 40,
-                    child: GridView.builder(
-                        padding: const EdgeInsets.all(0),
-                        semanticChildCount: 9,
-                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 1 / 3 * (_deviceWidth),
-                          // maxCrossAxisExtent: 353 / 3,
+                    Stack(
+                      children: [
+                        Container(
+                          // padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            // color: Colors.red.withOpacity(0.4),
+                          ),
+                          height: _deviceWidth - 40,
+                          child: GridView.builder(
+                              padding: const EdgeInsets.all(0),
+                              semanticChildCount: 9,
+                              gridDelegate:
+                                  SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 1 / 3 * (_deviceWidth),
+                                // maxCrossAxisExtent: 353 / 3,
+                              ),
+                              itemCount: 9,
+                              itemBuilder: (ctx, i) {
+                                return GridItem(
+                                    index: i,
+                                    marker: board[i],
+                                    buttonSelected: _runPlayersTurn);
+                              }),
                         ),
-                        itemCount: 9,
-                        itemBuilder: (ctx, i) {
-                          return GridItem(
-                              index: i,
-                              marker: board[i],
-                              buttonSelected: _runPlayersTurn);
-                        }),
-                  ),
-                  VerticalLines(deviceWidth: _deviceWidth),
-                  HorizontalLines(deviceWidth: _deviceWidth)
-                ]),
-                Expanded(
+                        VerticalLines(deviceWidth: _deviceWidth),
+                        HorizontalLines(deviceWidth: _deviceWidth)
+                      ],
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 50),
+                          CustomButton(
+                            width: 100 / 393 * _deviceWidth,
+                            height: 50,
+                            child: Text(
+                              'Restart',
+                              style: Theme.of(context)
+                                  .primaryTextTheme
+                                  .bodyText2!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            onPressed: _restartGame,
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Center(
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const SizedBox(height: 50),
+                      Text(_winnerMessage,
+                          style: Theme.of(context).primaryTextTheme.headline1),
+                      const SizedBox(height: 20),
                       CustomButton(
-                        width: 100 / 393 * _deviceWidth,
+                        width: 200 / 393 * _deviceWidth,
                         height: 50,
                         child: Text(
-                          'Restart',
+                          'Restart Game',
                           style: Theme.of(context)
                               .primaryTextTheme
                               .bodyText2!
                               .copyWith(color: Colors.white),
                         ),
-                        onPressed: _restartGame,
+                        onPressed: () {},
                       ),
-                      Spacer(),
+                      const SizedBox(height: 20),
+                      CustomButton(
+                        width: 200 / 393 * _deviceWidth,
+                        height: 50,
+                        child: Text(
+                          'Main Menu',
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .bodyText2!
+                              .copyWith(color: Colors.white),
+                        ),
+                        onPressed: () {},
+                      ),
                     ],
                   ),
                 ),
-              ],
-            ),
-          ),
         ),
       ),
     );
